0eae4bcc4d034e86ce417b9bb8649337bec4d2e3
pinchun <pinchunwang910930@gmail.com>
2025-12-26 17:56:37 +0800

    chore: fix project structure and ignore node_modules

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/e2e/integration/**/*.spec.ts'],
-  verbose: true,
-  setupFilesAfterEnv: ['./jest.setup.js'], // Optional, for global timeouts or setup
+  testMatch: ['<rootDir>/e2e/integration/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  }
 };
